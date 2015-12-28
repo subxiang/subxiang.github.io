5bdb6649af306b91eca39b440a19a13a60c8fb85
darkart <darkart@gmail.com>
2015-12-29 02:52:35 +0800

    fixed project list

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,3 +2,4 @@ $('body>div').addClass('container')
 $('header:first').addClass('page-header');
 $('footer:last').addClass('page-footer');
 $('header h1').addClass('capitalize');
+$('main dl:first').addClass('dl-horizontal');
